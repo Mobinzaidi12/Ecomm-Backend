a4c802d8c706bc6cee34069022ba74811a6292ba
Mobinzaidi12 <mobinzaidi2015@gmail.com>
2023-04-20 03:24:50 +0500

    formate document

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,28 +1,21 @@
-const jwt = require('jsonwebtoken');
-const jwtKey = 'e-comm'
+const jwt = require("jsonwebtoken");
+const jwtKey = "e-comm";
 
 function verifyToken(req, res, next) {
+  let token = req.headers["authorization"];
 
-    let token = req.headers['authorization']
-
-    if (token) {
-        token = token.split(' ')[1];
-        jwt.verify(token, jwtKey, (err,valid)=>{
-            if(err){
-                res.status(401).send({result: 'Please enter correct token'})
-            }else{
-                next() 
-            }
-            
-
-        })
-    } else {
-            res.status(402).send({result: 'Please enter token'})
-    }
-
-    
-
+  if (token) {
+    token = token.split(" ")[1];
+    jwt.verify(token, jwtKey, (err, valid) => {
+      if (err) {
+        res.status(401).send({ result: "Please enter correct token" });
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.status(402).send({ result: "Please enter token" });
+  }
 }
-
 
 module.exports = verifyToken;
